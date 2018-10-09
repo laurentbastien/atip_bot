1f73051377a30093ab958038e4b677285ff32552
laurent <laurentbastien@Laurents-MacBook-Pro.local>
2018-10-08 20:05:53 -0400

    update

--- Atip_tweeter.py
+++ Atip_tweeter.py
@@ -4,6 +4,8 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import base64
 import email
+from bs4 import BeautifulSoup
+
 from apiclient import errors
 from io import BytesIO
 
@@ -49,8 +51,6 @@ def GetMimeMessage(service, user_id, msg_id):
     message = service.users().messages().get(userId=user_id, id=msg_id,
                                              format='raw').execute()
 
-    print('Message snippet: %s' % message['snippet'])
-
     msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
 
     mime_msg = email.message_from_bytes(msg_str)
@@ -93,53 +93,64 @@ def ListMessagesMatchingQuery(service, user_id, query=''):
   except Exception as error:
     print('An error occurred: %s' % error)
 
-
+def get_message(id_query):
     
-def get_tweet_message(agency, id_query)
-  all_messages = {}
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
-  store = file.Storage('token.json')
-  creds = store.get()
-  if not creds or creds.invalid:
-      flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-      creds = tools.run_flow(flow, store)
-  service = build('gmail', 'v1', http=creds.authorize(Http()))
-
-  # Call the Gmail API
-  results = service.users().labels().list(userId='me').execute()
-  labels = results.get('labels', [])
-
-  user_id = "me"
-  all_messages["messages"] = ListMessagesMatchingQuery(service,user_id,query=id_query)
-  messageid = all_messages["messages"][0]["id"]
+    #id_query="A-2018-00155"    
 
 
-  GetMimeMessage(service,user_id,messageid)
+    all_messages = {}
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
 
-  finalmessage = GetMimeMessage(service,user_id,messageid)
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
 
-  b = email.message_from_string(str(finalmessage))
-  if b.is_multipart():
-      for payload in b.get_payload():
-          # if payload.is_multipart(): ...
-          print(payload.get_payload())
-  else:
-      messagetext = b.get_payload()
-
-  soup = BeautifulSoup(messagetext, 'html.parser')
-
-  data=[b.next_sibling for b in soup.findAll('br')]
-  tweet = data[17].replace("=\n","")
-
-  tweetagency = agency
-  ink = "du94lycnnfr2r.cloudfront.net/cdn-requests/"
-
-  return tweetagency+" "+tweet+" "+id_query+" "+"release."+" "+link+id_query+".pdf"
+    user_id = "me"
+    all_messages["messages"] = ListMessagesMatchingQuery(service,user_id,query=id_query)
+    messageid = all_messages["messages"][0]["id"]
 
 
-get_tweet_message("Insert the agency", "Insert the ATIP_ID")
+    GetMimeMessage(service,user_id,messageid)
 
+    finalmessage = GetMimeMessage(service,user_id,messageid)
+    
+    b = email.message_from_string(str(finalmessage))
+    if b.is_multipart():
+        for payload in b.get_payload():
+            # if payload.is_multipart(): ...
+            print(payload.get_payload())
+    else:
+        messagetext = b.get_payload()
 
+    soup = BeautifulSoup(messagetext, 'html.parser')
+    
+    data=[b.next_sibling for b in soup.findAll('br')]
+    tweet = data[17].replace("=\n","")
+    tweet = tweet.replace("=E2=80=93","")
+    the_agency = data[5].replace("=\n","")
 
+    link = "du94lycnnfr2r.cloudfront.net/cdn-requests/"
+    
+    if "Finance" in the_agency:
+        the_agency = "FIN"
+    if "Royal" in the_agency:
+        the_agency = "RCMP"
+    if "Environment" in the_agency:
+        the_agency = "ENV"
+    if "Northern" in the_agency:
+        the_agency = "INAC"
+        
+    if "Transport Canada" in the_agency:
+        the_agency = "TC"
+        
+    fullmessage = the_agency+". "+tweet+". "+id_query+" "+"release."+" "+link+id_query+".pdf" 
+
+    return fullmessage
